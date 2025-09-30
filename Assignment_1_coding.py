487720b6631a786ec9a285df1f14cb2a9d82cd6b
Isam Kartit <kartitisam@gmail.com>
2025-09-30 22:38:37 +0200

    Create Assignment_1_coding.py
    assignment for introduction to python programming, guessing game

---
+++ Assignment_1_coding.py
@@ -0,0 +1,33 @@
+import random
+
+repeat_game = "yes"
+
+
+while repeat_game == "yes":
+    number = random.randint(1,100)
+    tried = False
+    attempts = 0
+
+    print("I made up a number between 1 and 100, guess it with 5 tries if you can")
+
+    while attempts < 5 and not tried:
+        guess = int(input("Enter your guess"))
+        attempts += 1
+        if guess == number:
+            tried = True
+            print(f"congratulations you guessed the right number in {attempts} attempts!")
+        else:
+            if guess < number:
+                print("too low")
+            else:
+                print("too high")
+    if not tried:
+        print("sorry you didnt manage to guess the number")
+        print(f"the correct number was {number}")
+
+    repeat_game = input("Do you wanna play again? (yes/no)")
+    repeat_game = repeat_game.lower()
+
+print("good job, thanks for playing my game broski")
+
+            
