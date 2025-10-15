33d187a968d58d247f37c35136e70642b432d594
0303isamok <kartitisam@gmail.com>
2025-10-15 19:55:45 +0200

    math functions and counter added
    added a counter to the player and computer choices in the rock paper scissors game. also made some math functions.

---
+++ exercise/math_functions.py
@@ -0,0 +1,18 @@
+import math
+def cone_volume(height, radius):
+    grunnflate = radius**2*math.pi
+    formel = (grunnflate * height)/3
+    return round(formel, 2)
+result = cone_volume(2, 2)
+print(result)
+
+def poly(a, b, c, x):
+    return a*x**2 + b*x + c
+
+a = int(input("value for a"))
+b = int(input("value for b"))
+c = int(input("value for c"))
+x = int(input("value for x"))
+result = poly(a, b, c, x)
+
+print(result)
--- exercise/rockpaperscissors.py
+++ exercise/rockpaperscissors.py
@@ -1,5 +1,6 @@
 import random as rd
 
+
 def main():
     
         again = "yes"
@@ -7,6 +8,8 @@ def main():
         print("ROCK PAPER SCISSORS!")
         print("####################")
         shapes = ["rock", "paper", "scissors"]
+        player_count = 0
+        computer_count = 0
         
         while again == "yes":
             player_choice = get_player_choice(shapes)
@@ -14,6 +17,14 @@ def main():
             print(f"player: {player_choice} computer: {computer_choice}")
             result = winner(player_choice, computer_choice)
             print(result)
+            if result == "player won the game!":
+                player_count += 1
+            elif result == "computer won :-(":
+                computer_count += 1
+            else:
+                print("no one gets a point")
+
+            print(f"points:\n player:{player_count}\t computer:{computer_count}")
 
             again = input("do you want to play again? (yes/no)").strip().lower()
             while again not in ("yes", "no"):
@@ -41,7 +52,8 @@ def winner(player_choice, computer_choice):
 
     elif player_choice == "rock" and computer_choice == "scissors" or player_choice == "scissors" and computer_choice == "paper" or player_choice == "paper" and computer_choice == "rock":
         return "player won the game!"
+
     else:
         return "computer won :-("
-    
+
 main()
