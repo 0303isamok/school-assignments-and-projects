6a82d2cb22946642ebf54e8a2626d6c13978f714
0303isamok <kartitisam@gmail.com>
2025-10-15 13:17:07 +0200

    Update project1.py
    fix for invalit awnsers when asked to play again

--- school_assignments/project1.py
+++ school_assignments/project1.py
@@ -2,35 +2,39 @@
 import random
 
 repeat_game = "YES" 
+if repeat_game == "YES":
+    while repeat_game == "YES":
+        number = random.randint(1,100)
+        tried = False
+        attempts = 0
 
+        print("I made up a whole number between 1 and 100 (no decimals), guess it with 5 tries")
 
-
-while repeat_game == "YES":
-    number = random.randint(1,100)
-    tried = False
-    attempts = 0
-
-    print("I made up a whole number between 1 and 100 (no decimals), guess it with 5 tries")
-
-    while attempts < 5 and not tried:
-        guess = int(input("Enter your guess:"))
-        attempts += 1
-        if guess == number:
-            tried = True
-            print("Congratulations you guessed the right number in ", attempts, " attempts!")
-            print("The correct number was ", number,"!")
-        else:
-            if guess < number:
-                print("Too low")
-                print(5 - attempts, "attempts remaining")
+        while attempts < 5 and not tried:
+            guess = int(input("Enter your guess:"))
+            attempts += 1
+            if guess == number:
+                tried = True
+                print("Congratulations you guessed the right number in ", attempts, " attempts!")
+                print("The correct number was ", number,"!")
             else:
-                print("Too high")
-                print(5 - attempts, "attempts remaining")
+                if guess < number:
+                    print("Too low")
+                    print(5 - attempts, "attempts remaining")
+                else:
+                    print("Too high")
+                    print(5 - attempts, "attempts remaining")
 
-    if not tried:
-        print("You did not manage to guess the number. You have reached the guessing limit")
-        print("The correct number was", number)
-    repeat_game = input("Do you wanna play again? (YES/NO)")
-    repeat_game = repeat_game.upper()
+        if not tried:
+            print("You did not manage to guess the number. You have reached the guessing limit")
+            print("The correct number was", number)
+        repeat_game = str(input("Do you wanna play again? (YES/NO)"))
+        repeat_game = repeat_game.upper()
+    if repeat_game == "NO":
+        print("ok")
+    else:
+        print("invalid awnser")
+
+
     
 print("Good job, thanks for playing my game")
