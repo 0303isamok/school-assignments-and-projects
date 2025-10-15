621ddb00a4bd2bc9f13f4f5d11bc25ed5353741a
0303isamok <kartitisam@gmail.com>
2025-10-15 17:48:16 +0200

    Update test.py

--- exercise/test.py
+++ exercise/test.py
@@ -1,12 +1,47 @@
+import random as rd
+
+def main():
+    again = "yes"
+    while again == "yes":
+        print("####################")
+        print("ROCK PAPER SCISSORS!")
+        print("####################")
+        shapes = ["rock", "paper", "scissors"]
+        
+        while True:
+            player_choice = get_player_choice(shapes)
+            computer_choice = get_computer_choice(shapes)
+            print(f"player: {player_choice} computer: {computer_choice}")
+            result = winner(player_choice, computer_choice)
+            print(result)
+
+            again = input("do you want to play again? (yes/no)").strip().lower()
+            while again != "yes" and again != "no":
+                print("invalid user input")
+                again = input("do you want to play again? (yes/no)").strip().lower()
+            if again == "no":
+                break
+        
 
 
-shapes = {
-    "rock": 1,
-    "paper": 2,
-    "scissors": 3
-}
-p1 = input("choose a shape:")
-p2 = input("choose a shape:")
+def get_computer_choice(shapes):
+    return rd.choice(shapes)
 
-if p1 == p2:
-    print("its a tie")
+def get_player_choice(shapes):
+    while True:
+        player_choice = input("you can choose between\n what do you want to throw?")
+        if player_choice in shapes:
+            return player_choice
+        else:
+            return "invalid user input"
+
+def winner(player_choice, computer_choice):
+    if player_choice == computer_choice:
+        return "its a tie!"
+
+    elif player_choice == "rock" and computer_choice == "scissors" or player_choice == "scissors" and computer_choice == "paper" or player_choice == "paper" and computer_choice == "rock":
+        return "player won the game!"
+    else:
+        return "computer won :-("
+    
+main()
