d4d495ac5e6420ed23e1b3b5c064b9810bb81f45
0303isamok <kartitisam@gmail.com>
2025-10-31 12:43:04 +0100

    Update warrior_game.py
    assassin chilld-class and vagabond child-class

--- exercise/warrior_game.py
+++ exercise/warrior_game.py
@@ -5,7 +5,34 @@ class Character():
     
     def take_damage(self, amount):
         self.health -= amount
+    
 
-warrior_1 = Character(100, 20)
-warrior_1.take_damage(40)
-print(warrior_1.health)
+class Assassin(Character):
+    def __init__(self, health, damage):
+        super().__init__(health, damage)
+        self.toughness = 0.90
+    def take_damage(self, amount):
+        modified_amount_armor = amount * self.toughness
+        super().take_damage(modified_amount_armor)
+class Vagabond(Character):
+    def __init__(self, health, damage):
+        super().__init__(health, damage)
+        self.critical_strike = 1.50
+    def take_damage(self, amount):
+        modified_amount_crit = amount * self.critical_strike
+        super().take_damage(modified_amount_crit)
+        
+
+
+
+assassin = Assassin(100, 20)
+vagabond = Vagabond(100, 20)
+print("Assassin's health before combat")
+print(assassin.health)
+assassin.take_damage(40)
+print("Vagabond's health before combat")
+print(vagabond.health)
+vagabond.take_damage(30)
+
+print(assassin.health)
+print(vagabond.health)
