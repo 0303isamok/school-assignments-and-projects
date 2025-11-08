7b166a8aabec8b307d8258a82e02270fd9edfd2f
Isam Kartit <kartitisam@gmail.com>
2025-11-09 00:44:19 +0100

    Update test.py

--- exercise/test.py
+++ exercise/test.py
@@ -1,3 +1,9 @@
-for i in range (1,6):
-    print("*"*i)
-    
+#list1 = (2, "we", None, ["hello", 2])
+#list1 = list1.append(["yurr", 5])
+#print(list1)
+#Error message because tuples are imutable
+lis = []
+tup = (23, "tttt")
+
+lis.append("ttt")
+print(lis[0])
