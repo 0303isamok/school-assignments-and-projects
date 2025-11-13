aae691171d4c3241e09d8e93aaed6904196565c4
Esmaaa <kartitisam@gmail.com>
2025-11-13 01:13:28 +0100

    tkinter learning

--- exercise/tkint.py
+++ exercise/tkint.py
@@ -4,13 +4,18 @@ import tkinter as tk
 root = tk.Tk()
 
 # Set the window title
-root.title("My First Tkinter Window")
+root.title("Goat")
 
 # Set the window size (width x height)
-root.geometry("500x400")
+root.geometry("500x500")
 
 # Set the background color
 root.configure(bg="lightgray")
+
+label = tk.Label(root, text="I Like Fridge", font=("Arial", 18))
+label.pack()
+
+icon_image = tk.PhotoImage(file="duckling_1.png")
 
 # Start the Tkinter event loop
 root.mainloop()
