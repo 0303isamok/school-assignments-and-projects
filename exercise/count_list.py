d5540c810f8a48362ad3345ae39d5c3eca1e30d0
0303isamok <kartitisam@gmail.com>
2025-11-16 01:26:42 +0100

    finishing File organizer

--- exercise/count_list.py
+++ exercise/count_list.py
@@ -1,9 +1,12 @@
-list1 = [2, 5, 5, 6, 7, 3, 2, 8, 2, 2, 6]
+# list1 = [2, 5, 5, 6, 7, 3, 2, 8, 2, 2, 6]
 
-count = 0
-for i in list1:
-    if i == 2:
-        count = count + 1
+# count = 0
+# for i in list1:
+#     if i == 2:
+#         count = count + 1
     
-#print(count)
-print(list1[-1])
+# #print(count)
+# print(list1[-1])
+
+list1 = [None]
+print(list1[0])
--- exercise/file_organiser.py
+++ exercise/file_organiser.py
@@ -16,22 +16,22 @@ extensions = {
     ".AVI": "video_1"
 }
 
-for filename in os.listdir(directory):
-    file_path = os.path.join(directory, filename)
+for name_file in os.listdir(directory):
+    file_path = os.path.join(directory, name_file)
 
     if os.path.isfile(file_path):
-        extension = os.path.splitext(filename)[1].lower()
+        extension = os.path.splitext(name_file)[1].lower()
 
         if extension in extensions:
             folder_name = extensions[extension]
 
             folder_path = os.path.join(directory, folder_name)
             os.makedirs(folder_path, exist_ok = True)
-            destination_path = os.path.join(folder_path, filename)
+            destination_path = os.path.join(folder_path, name_file)
             shutil.move(file_path, destination_path)
 
-            print(f"Moved {filename} to {folder_name} folder.")
+            print(f"Moved {name_file} to {folder_name} folder.")
         else:
-            print(f"skipped {filename}. Unknown file extension")
+            print(f"skipped {name_file}. Unknown file extension")
     else:
-        print(f"Skipped {filename}, it is a directory")
+        print(f"Skipped {name_file}, it is a directory")
